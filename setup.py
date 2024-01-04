ad5d3f159018d5532fd80e9a677d33d6423199bc
Alex Manning <alexander.manning@stfc.ac.uk>
2024-01-04 14:15:20 +0000

    Fix loading of cookielaw.

--- setup.py
+++ setup.py
@@ -11,36 +11,35 @@ try:
 except ImportError:
     # If we get an import error, find the version string manually
     version = "unknown"
-    with open(os.path.join(here, 'fwtheme_django_jasmin', '__init__.py')) as f:
+    with open(os.path.join(here, "fwtheme_django_jasmin", "__init__.py")) as f:
         for line in f:
-            match = re.search('__version__ *= *[\'"](?P<version>.+)[\'"]', line)
+            match = re.search("__version__ *= *['\"](?P<version>.+)['\"]", line)
             if match:
-                version = match.group('version')
+                version = match.group("version")
                 break
 
-with open(os.path.join(here, 'README.md')) as f:
+with open(os.path.join(here, "README.md")) as f:
     README = f.read()
 
 if __name__ == "__main__":
-
     setup(
-        name = 'fwtheme-django-jasmin',
-        version = version,
-        description = 'Framework-level theme for Django customised for JASMIN app using bootstrap 4',
-        long_description = README,
-        classifiers = [
+        name="fwtheme-django-jasmin",
+        version=version,
+        description="Framework-level theme for Django customised for JASMIN app using bootstrap 4",
+        long_description=README,
+        classifiers=[
             "Programming Language :: Python",
             "Framework :: Django",
             "Topic :: Internet :: WWW/HTTP",
             "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
-        author = 'Matt Pritchard',
-        author_email = 'matt.pritchard@stfc.ac.uk',
-        url = 'https://github.com/cedadev/fwtheme-django-jasmin',
-        keywords = 'web django theme bootstrap',
-        packages = find_packages(),
-        include_package_data = True,
-        zip_safe = False,
-        install_requires = [ ],
-        extras_require = { },
+        author="Matt Pritchard",
+        author_email="matt.pritchard@stfc.ac.uk",
+        url="https://github.com/cedadev/fwtheme-django-jasmin",
+        keywords="web django theme bootstrap",
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[],
+        extras_require={},
     )
